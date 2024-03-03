9b6d544bc5b41805019fc48de59828cd268f72e0
Agravion <riccardotomassoni@gmail.com>
2024-03-03 16:31:26 +0100

    fixed the rudimentary binary save and load sistem

--- Scenes/World/World.gd
+++ Scenes/World/World.gd
@@ -11,10 +11,7 @@ var cubeMaterial;
 var cubeRIDs = [];
 
 func _ready():	
-	SaveWord.save()
-	SaveWord.load()
-	
-	# prepare the material
+# prepare the material
 	cubeMaterial = StandardMaterial3D.new();
 	cubeMaterial.albedo_color = Color(1.0, 1.0, 1.0);
 	cubeMaterial.albedo_texture = texture;
